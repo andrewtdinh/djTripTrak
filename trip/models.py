06f4ae3e830237c75afb77c2ed070af8bb9efdf8
Andrew Dinh <andrewdinh74@yahoo.com>
2025-11-15 14:19:52 -0800

    Add the Note model

--- trip/models.py
+++ trip/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
 from django.contrib.auth.models import get_user_model
 
 User = get_user_model()
@@ -15,3 +15,19 @@ class Trip(models.Model):
     def __str__(self):
         return self.city
     
+class Note(models.Model):
+    EXCURSIONS = (
+        ("event", "Event"),
+        ("dining", "Dining"),
+        ("experience", "Experience"),
+        ("general", "General"),
+    )
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='notes')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=100, choices=EXCURSIONS)
+    img = models.ImageField(upload_to='notes', blank=True, null=True)
+    rating = models.PositiveSmallIntegerField(default=1, validators=[MaxValueValidator(5)]) 
+
+    def __str__(self):
+        return f"{self.name} in {self.trip.city}"

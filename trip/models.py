2aad5f3f52419e2ee54222fe8b2f6211804915cd
Andrew Dinh <andrewdinh74@yahoo.com>
2025-11-17 13:26:01 -0800

    Make and execute migration

--- trip/models.py
+++ trip/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
-from django.contrib.auth.models import get_user_model
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 

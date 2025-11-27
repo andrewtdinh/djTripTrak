07fa59d40489b6e0cf7294760fb4dbddea8ea604
Andrew Dinh <andrewdinh74@yahoo.com>
2025-11-27 08:40:09 -0800

    Define the form to create a new trip

--- trip/views.py
+++ trip/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Trip, Note
 
@@ -14,5 +15,16 @@ def trips_list(request):
         'trips': trips,
     }
     return render(request, 'trip/trips_list.html', context)
+
+class TripCreateView(CreateView):
+    model = Trip
+    success_url = reverse_lazy('trips-list')
+    fields = ['city', 'country', 'start_date', 'end_date']
+    # It automatically looks for template named mode_form.html
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+    
 
 

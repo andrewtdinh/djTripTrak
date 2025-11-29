738af59cfd6f1bad14443e224271ca3e4b58f745
Andrew Dinh <andrewdinh74@yahoo.com>
2025-11-29 13:35:48 -0800

    Add Note Detail View

--- trip/views.py
+++ trip/views.py
@@ -37,6 +37,11 @@ class TripDetailView(DetailView):
         notes = trip.notes.all()
         context['notes'] = notes
         return context
+    
+class NoteDetailView(DetailView):
+    model = Note
+
+    
 
 
 

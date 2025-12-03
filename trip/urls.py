3129d7e9d093cb7f0bb1f5a473cd95cdc9b764c6
Andrew Dinh <andrewdinh74@yahoo.com>
2025-12-03 15:02:13 -0800

    Remove bad file and attach another file

--- trip/urls.py
+++ trip/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomeView, trips_list, TripCreateView, TripDetailView, NoteDetailView, NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView
+from .views import HomeView, trips_list, TripCreateView, TripDetailView, NoteDetailView, NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView, TripUpdateView, TripDeleteView
+
 
 
 urlpatterns = [
@@ -9,8 +10,10 @@ urlpatterns = [
     path('dashboard/note/create/', NoteCreateView.as_view(), name='note-create'),
     path('dashboard/trip/create/', TripCreateView.as_view(), name='trip-create'),
     path('dashboard/trip/<int:pk>/', TripDetailView.as_view(), name='trip-detail'),
+    path('dashboard/trip/<int:pk>/update/', TripUpdateView.as_view(), name='trip-update'),
+    path('dashboard/trip/<int:pk>/delete/', TripDeleteView.as_view(), name='trip-delete'),
+    path('dashboard/trip/<int:pk>/', TripDetailView.as_view(), name='trip-detail'),
     path('dashboard/note/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
     path('dashboard/note/<int:pk>/update/', NoteUpdateView.as_view(), name='note-update'),
     path('dashboard/note/<int:pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
-
 ]
